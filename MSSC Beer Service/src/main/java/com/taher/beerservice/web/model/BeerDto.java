db0fb82266c48848872b814e1aca03d6e07b7568
TaherVora <vorataher21@gmail.com>
2023-05-02 10:58:38 -0700

    Added validation. Closes#8

--- MSSC Beer Service/src/main/java/com/taher/beerservice/web/model/BeerDto.java
+++ MSSC Beer Service/src/main/java/com/taher/beerservice/web/model/BeerDto.java
@@ -1,6 +1,10 @@
 package com.taher.beerservice.web.model;
 
 //import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,13 +19,32 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
+    @Positive
     private Long upc;
+
+    @NotNull
+    @Positive
     private BigDecimal price;
+
     private Integer quantityOnHand;
 }
